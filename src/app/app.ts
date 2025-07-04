d5ae9c5a2ec596c43ffd6149016ace24f00df685
jorenVDZ <jorenvanderzande@hotmail.com>
2025-07-04 22:25:35 +0200

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { AboutPage } from './pages/about/about-page/about-page';
-import { ContactPage } from './pages/contact/contact-page/contact-page';
-import { HomePage } from './pages/home/home-page/home-page';
-import { ProjectsPage } from './pages/projects/projects-page/projects-page';
-import { SkillsPage } from './pages/skills/skills-page/skills-page';
-import { Navigation } from './shared/components/navigation/navigation';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
-    HomePage,
-    AboutPage,
-    ProjectsPage,
-    SkillsPage,
-    ContactPage,
-    Navigation
+    RouterOutlet
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
