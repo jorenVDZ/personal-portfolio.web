a653139e43fa0e19a42c4b9169e9384ea526bca1
jorenVDZ <jorenvanderzande@hotmail.com>
2025-07-04 22:28:47 +0200

    back to original

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { MainPage } from './pages/main/main-page/main-page';
 
-export const routes: Routes = [
-  { path: '', component: MainPage },
-  { path: '**', redirectTo: '' }
-];
+export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AboutPage } from './pages/about/about-page/about-page';
+import { ContactPage } from './pages/contact/contact-page/contact-page';
+import { HomePage } from './pages/home/home-page/home-page';
+import { ProjectsPage } from './pages/projects/projects-page/projects-page';
+import { SkillsPage } from './pages/skills/skills-page/skills-page';
+import { Navigation } from './shared/components/navigation/navigation';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
+    HomePage,
+    AboutPage,
+    ProjectsPage,
+    SkillsPage,
+    ContactPage,
+    Navigation
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
